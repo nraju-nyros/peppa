d44632b82d44b527bca905529715a1965f6f435a
nraju-nyros <49183724+nraju-nyros@users.noreply.github.com>
2020-01-05 11:17:12 +0530

    Update 20190726060317_addlatitude_to_location.rb

--- db/migrate/20190726060317_addlatitude_to_location.rb
+++ db/migrate/20190726060317_addlatitude_to_location.rb
@@ -1,6 +1,6 @@
 class AddlatitudeToLocation < ActiveRecord::Migration[5.2]
   def change
     add_column :locations, :latitude, :float
-    add_column :locations, :langitude, :float
+    add_column :locations, :longitude, :float
   end
 end
